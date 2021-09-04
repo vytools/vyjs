16d729bfbb752a6454a4b40d1ac29e4f65a5f171
Nate Bunderson <nate.bunderson@asirobots.com>
2021-09-04 16:05:55 -0600

    Draw images

--- generic_map.js
+++ generic_map.js
@@ -1,8 +1,20 @@
-import { initialize_map } from "https://cdn.jsdelivr.net/gh/natebu/jsutilities@v0.1.3/zoom_pan_canvas.js";
+import { initialize_map } from "https://cdn.jsdelivr.net/gh/natebu/jsutilities@v0.1.5/zoom_pan_canvas.js";
 
 // allowable objects:
-// polygons => {draw_type:'polygon',points:[{x:0,y:0},{x:200,y:0},{x:200,y:200}],fillStyle:'red',strokeStyle:'rgba(0,0,0,0.5)'}
-// circles => {draw_type:'circle',x:0,y:0,radius:10,fillStyle:'red',strokeStyle:'rgba(0,0,0,0.5)'}
+// polygons => {draw_type:'polygon', points:[{x:0,y:0},{x:200,y:0},{x:200,y:200}],fillStyle:'red',strokeStyle:'rgba(0,0,0,0.5)'}
+// circles => {draw_type:'circle', x:0, y:0, radius:10, fillStyle:'red',strokeStyle:'rgba(0,0,0,0.5)'}
+// images => {draw_type:'image', x:0, y:0, w:100, h:100, rotation:1.57, image:img1}
+    // x, y, position of bottom left corner, positive is rotation counter-clockwise around that point
+    // var img1 = new Image();
+    // img1.src = 'data:image/png;base64,' + 'iVBORw0KGgoAAAANSUhEUgAAABIAAAASCAMAAABhEH5lAAAABGdBTUEAALGPC/xhBQAAAa1QTFRF5H444Ho14Xo15oQ45YQ45oI56os854Y443423HYtAAAA3HctzGAT9plE+qZM/eHE+bt///36/dy6+bVz+aFH/OHI/Nm1+aRP/vXr//jw5o9p/N/E/vPo9Kpw9bWD/vPq/vTs/enX/erW/MqV/ObT+8aR9ZM5+Zs7+sma4XRC+dGv98CR+LFs/dWt/vDh+9Ov+uDN+uDP/vj0/fDo9JRA8JFG9beG+ta686ds+6hQ+adV9ruN5oJN/PLt9MKj53Yx+qBC+J1C7qN599bD6IVM/O3h8Jxa97Bu//79+7Vu+Ld787mW/fTt8Jtf6oE78pA795c5/fPs64I77JRZ/NOq/Nu8/Lhv/unU/u7d3WQv4no343s3///++Zw995k843w3/vXt5XYy64s75H045oE45X846oo75oI554Q55YA46IU66IQ65oE56ok66IY654M56Yg62l0p3GAq3WMr5XMu6nwx53gv4Gcs/vv54mwt64Ay7YQz5HAu74c053Uv6Yc6+pw5+Jg48Ys084416Xow9JI27H8y9ZI3+Zo59pU374Y0+54695c48ow1////AGx8/AAAAA10Uk5T/fn5+fn9/fn5ywDLKOfV8VwAAAEpSURBVBjTVc5TcwMBFAXgWyVZ1LZt27YV2+aGm0YNNvc3d9PMdKbn8Xs45wBJEyKtwWi2s06bXi2kaBLoGpXX7wuFo4lkOsdlZQIaCJX3+elP4gU5BSKv/1F6uj69Upav72rQ+n03yKdnpixFFgy+0Nna1CCidPagJHkLGEPhVb6nuQNx7pCXjAPM4ahyoLuBa2lCnOclZQV7NKHkq+rrCm2IG5lUzARsInk+MYTY2V7bj8t3sYgOnKU/48ON2FrsRbyOBDVgS+ceLjcLo4h9mUU8DgYUoM9xL3ixNTaCC6k93A14JKDmsu+IV8VtXNo/Qpx0i0GYjb99vN7nbxnmZIdhulwVQMl+P/PrkWDA43Z9EkAL5P8EaCBpqoq1OKwmnUYhEVcSNPkD6BN5Z82OyewAAAAASUVORK5CYII=';
+    
+let draw_image = function(img, ctx) {
+  ctx.save();
+  ctx.translate(img.x, img.y);
+  ctx.rotate(img.rotation);
+  ctx.drawImage(img.image, 0, 0, img.w, img.h);
+  ctx.restore();
+}
 
 let draw_polygon = function(poly, ctx) {
   if (poly.points.length < 2) return;
@@ -35,6 +47,8 @@ let draw_thing = function(thing, ctx) {
         draw_polygon(thing,ctx);
       } else if (thing.draw_type == 'circle') {
         draw_circle(thing,ctx);
+      } else if (thing.draw_type == 'image') {
+        draw_image(thing,ctx);
       }
     } else {
       for (const [key, val] of Object.entries(thing)) {
