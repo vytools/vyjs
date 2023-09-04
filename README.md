4a407983f11d7a858ae4655848f9e558c881ff22
Nate Bunderson <nate@vy.tools>
2023-09-04 14:35:24 -0600

    Fix display results

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # vyjs
 A group of javascript modules to make creating user interfaces simpler
-You can use jsdelivr.net to get the latest modules. For example the module generic_map.js can be accessed in your javascript with (assuming a commit tag of 2.0.7 exists for this repo)
+You can use jsdelivr.net to get the latest modules. For example the module generic_map.js can be accessed in your javascript with (assuming a commit tag of 2.0.8 exists for this repo)
 
-import { setup_generic_map } from "https://cdn.jsdelivr.net/gh/vytools/vyjs@v2.0.7/js/generic_map.js";
+import { setup_generic_map } from "https://cdn.jsdelivr.net/gh/vytools/vyjs@v2.0.8/js/generic_map.js";
 
 
 ## generic_map.js
--- js/display_results.js
+++ js/display_results.js
@@ -2,31 +2,40 @@ import { dynamic_chart } from './chartjs.js';
 
 const make_problem_table = function(problem) {
   let inputs = '', outputs = '';
-  for (const [key,val] of Object.entries(problem.inputs)) {
-    inputs += `<tr><td>${key}</td><td>${JSON.stringify(val)}</td></tr>`;
+  if (problem.inputs) {
+    for (const [key,val] of Object.entries(problem.inputs)) {
+      inputs += `<tr><td>${key}</td><td>${JSON.stringify(val)}</td></tr>`;
+    }
   }
   problem.points_earned = 0;
   problem.points_possible = 0;
   for (const [key,val] of Object.entries(problem.outputs)) {
-    problem.points_earned += parseFloat(val.points_earned);
-    problem.points_possible += parseFloat(val.points_possible);
-    let cls = (val.points_earned == val.points_possible) ? 'table-success' : 'table-danger';
+    let points = `<td>${val.points_possible}</td>`;
+    if (val.points_earned) {
+      problem.points_earned += parseFloat(val.points_earned);
+      problem.points_possible += parseFloat(val.points_possible);
+      let cls = (val.points_earned == val.points_possible) ? 'table-success' : 'table-danger';
+      points = `<td class="${cls}">${parseFloat(val.points_earned)}/${parseFloat(val.points_possible)}</td>`;
+    }
     if (val.points_possible > 0) {
       outputs += `<tr>
           <td>${key}</td>
           <td>${val.expected}</td>
-          <td>${val.actual}</td><td>${val.tolerance}</td>
-          <td class="${cls}">${parseFloat(val.points_earned)}/${parseFloat(val.points_possible)}</td>
+          <td>${val.actual}</td><td>${val.tolerance}</td>${points}
         </tr>`;
     }
+
   }
-  let cls = (problem.points_possible == problem.points_earned) ? 'bg-success' :
-    (problem.points_earned == 0) ? 'bg-danger' : 'bg-warning'; 
+  let badge = '';
+  if (problem.points_possible > 0) {
+    let cls = (problem.points_possible == problem.points_earned) ? 'bg-success' :
+              (problem.points_earned == 0) ? 'bg-danger' : 'bg-warning';
+    badge = `<span class="badge ${cls}" style="float:right">${problem.points_earned} earned / ${problem.points_possible} possible</span>`;
+  }
   return `
     <table class="table border border-dark">
       <thead class="table-dark">
-        <td>${problem.name}
-        <span class="badge ${cls}" style="float:right">${problem.points_earned} earned / ${problem.points_possible} possible</span></td>
+        <td>${problem.name}${badge}</td>
       </thead>
       <tbody>
         <tr class="p-0 m-0">
@@ -54,29 +63,30 @@ export function process_results(stdout, results, elmnt, is_offline) {
     let points_earned =0, points_possible = 0, html = ''
     if (stdout) {
         html = `<h4 style="text-align:center">Stdout</h4>
-            <div class="alert alert-secondary"><pre>${r.stdout}</pre></div>`;
+            <div class="alert alert-secondary"><pre>${stdout}</pre></div>`;
     }
     let results_html = '';
-    if (results && results.problems) {
-        for (var i = 0; i < results.problems.length; i++) {
-            results_html += make_problem_table(results.problems[i]);
-            points_earned += results.problems[i].points_earned;
-            points_possible += results.problems[i].points_possible;
-        }
-    }
     let offline = `<a class="btn btn-dark btn-sm" onclick="vysubmit()">Submit to vy.tools for grading</a>`;
-    html += `
-<h4 style="text-align:center">Results</h4>
-<div class="alert alert-dark" role="alert" style="text-align:center;">
-    <h5>Earned <span style="font-weight:bold; font-color:red">${(points_earned/points_possible*100).toFixed(0)}%</span> of possible points</h5>
-    ${(is_offline) ? offline : ''}
-</div>${DOMPurify.sanitize(results_html)}`;
+    let summary = '';
+    if (results && results.problems) {
+      for (var i = 0; i < results.problems.length; i++) {
+        results_html += make_problem_table(results.problems[i]);
+        points_earned += results.problems[i].points_earned;
+        points_possible += results.problems[i].points_possible;
+      }
+      if (results.ran) {
+        summary = `<div class="alert alert-dark" role="alert" style="text-align:center;">
+        <h5>Earned <span style="font-weight:bold; font-color:red">${(points_earned/points_possible*100).toFixed(0)}%</span> of possible points</h5>
+        ${(is_offline) ? offline : ''}</div>`;
+      }
+    }
+    html += `<h4 style="text-align:center">Tests</h4>${summary}${DOMPurify.sanitize(results_html)}`;
     elmnt.insertAdjacentHTML('beforeend',html);
     if (results && results.plots) {
-        elmnt.insertAdjacentHTML('beforeend','<h4 style="text-align:center">Plots</h4>');
-        for (var i = 0; i < results.plots.length; i++) {
-          // TODO is there any way at all to DOMPurify this?  
-          dynamic_chart(results.plots[i], `chart${i}`, elmnt, ()=>{});
-        }
+      elmnt.insertAdjacentHTML('beforeend','<h4 style="text-align:center">Plots</h4>');
+      for (var i = 0; i < results.plots.length; i++) {
+        // TODO is there any way at all to DOMPurify this?  
+        dynamic_chart(results.plots[i], `chart${i}`, elmnt, ()=>{});
+      }
     }
 }
